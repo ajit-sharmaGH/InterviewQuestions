adc783c9c5c81b576144ce928d68cdbb2548ccec
AJIT SHARMA <ajit08082000@gmail.com>
2021-07-06 12:49:22 +0530

    updated the code and added some extra questions..!!

--- interview.js
+++ interview.js
@@ -7,21 +7,21 @@ const chalk = require("chalk");
         chalk.bgWhite.red("May I have your name first ? ")
     );
     var score = 0;
-    console.log(userName);
+    // console.log(userName);
 
     var welcomeMessage = "welcome " + userName;
     console.log(welcomeMessage);
     var userAnswerAge = readlineSync.question(
-        "Are you older than 30? if yes press Y else N : "
+        "Are you older than 30? if yes press Y for yes or N for no : "
     );
     console.log("You enterd " + userAnswerAge);
     if (userAnswerAge === "N") {
-        console.log(chalk.green("you are right just proceed ahead "));
+        console.log(chalk.green("you are right here eligible for interview just proceed ahead "));
         score = score + 1;
         var userCurrentAge = readlineSync.question(
             " Tell me your current age in words !"
         );
-        console.log(userCurrentAge);
+        console.log("you entered " + userCurrentAge);
     } else {
         console.log(
             chalk.green(
@@ -43,7 +43,7 @@ const chalk = require("chalk");
 
         console.log(chalk.green("Now your score is : " + score));
     } else {
-        console.log(chalk.bgRed("You need at least a guider like mr. Ayan "));
+        console.log(chalk.bgRed("You need at least a guider like a good and experiance teacher "));
         score = score - 1;
 
         console.log("score :" + score);
@@ -64,44 +64,41 @@ const chalk = require("chalk");
         if (userAns == answer) {
             score++;
             console.log(chalk.bgYellow("Correct answer"));
+
+            var enter = readlineSync.question(" Press ENTER to proceed : ");
+            console.log(enter);
+
+
         } else {
             console.log(
                 chalk.bgRed("Your answer is wrong, the correct answer is " + answer)
             );
+
+            var enter = readlineSync.question(" Press ENTER to proceed : ");
+            console.log(enter);
+
+
         }
     }
 
-    /*function qna(question, answer){
-        
-         // console.log("numberOne :",  numberOne, "numberTwo :", numberTwo)
-         var userAnswer = readlineSync.question(question);
-         
-       }
-         if(userAns==="answer"){
-         score++;
-         console.log("You are right :\n Go for the next task \n");
-       }
-        else {
-         console.log("Your answer is wrong, the correct ans is :" + answer);
-       }
-       */
 
-    // use the funtion
 
-    //var aboveAnswer = readlineSync.question("If above ans. is correct type Y if no type N :\n");
-    //console.log("You enterd  " + aboveAnswer);
 
-    //console.log(aboveAnswer);
 
-    qna(" 1. Sum of 150 and 270 ? ", "420");
-    // console.log("The sum of the numbers : " + result);
+    qna(" 1. What is the output of this code ?\n function test(x,y){\n if(x>y){\n document.write(x);\n} else{\n document.write(y);\n }\n}\n test(5,8); \n type your ans here : ", "8");
 
-    //console.log("Your second task goes here :");
 
     qna(
-        " 2. Who is the current CEO of GOOGLE ?: Please type the opt only\n a - Satya Nadela,\n b -Bill Gates,\n c -Sundar Pichai,\n d - Non of the above ",
+        " 2. Who is the current CEO of GOOGLE ?: Please type the opt only\n a - Satya Nadela,\n b -Bill Gates,\n c -Sundar Pichai,\n d - Non of the above \n type the opt here : ",
         "c"
     );
+    qna(
+        "3. What is the output of the following code ?\n function test(number)\n{\n while(number<5){\n number++;\n}\n return number;\n}\n alert(test(2));\n Type your ans here : ", "5"
+    );
+
+    qna(
+        "4. What shape results will form from the following code ?\n <svg width=''100'' height=''100''>\n <line x1=''50'' y1=''0'' x2=''50'' y2= ''100'' \n style=''stroke:black''/>\n <line x1=''0'' y1=''50'' x2=''100'' y2=''50'' \n style=''stroke:black''/>\n </svg>\n a. plus sign\n b. minus sign\n c. perpendicular\n d. none of these\n type your ans here :  ", "a");
+
 
     console.log("You scored " + score);
 
@@ -116,7 +113,7 @@ const chalk = require("chalk");
         console.log(loading);
         console.log(
             chalk.bgRed(
-                "Sorry to say \n You are rejected !\n You need to score at least 3 marks"
+                "Sorry to say \n You are rejected !\n You need to score at least 3 marks \n our team suggests -- plz go through the revision and check where did you mistaken "
             )
         );
     }
